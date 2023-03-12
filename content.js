fdc65f774427dd8208b03a6fc32b09aaf2a4e517
Ali Elzein <alielzein753@gmail.com>
2023-03-12 10:34:22 -0400

    hello darkness my old friend

--- content.js
+++ content.js
@@ -1,48 +1,77 @@
-const tagSet = new Set([
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6',
-    'p',
-    'span',
-    'br',
-    'sup',
-    'sub',
-    'u',
-    'i',
-    'b',
-    'em',
-    'strong',
-    'del',
-    'ins',
-    'ruby',
-    'kbd',
-    'wbr',
-]);
-const targetElements = Array.from(document.querySelectorAll('*')).filter(
-    (d) =>
-        tagSet.has(d.tagName.toLowerCase()) &&
-        d.textContent.split(' ').length >= 20
-);
+function transformText() {
+    const tagSet = new Set([
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'p',
+        'span',
+        'br',
+        'sup',
+        'sub',
+        'u',
+        'i',
+        'b',
+        'em',
+        'strong',
+        'del',
+        'ins',
+        'ruby',
+        'kbd',
+        'wbr',
+    ]);
+    const targetElements = Array.from(document.querySelectorAll('*')).filter(
+        (d) =>
+            tagSet.has(d.tagName.toLowerCase()) &&
+            d.textContent.split(' ').length >= 20
+    );
 
-for (const te of targetElements) {
-    console.log(te.textContent);
-    const words = te.textContent.split(' ');
-    let result = [];
-    for (const word of words) {
-        const middle = Math.floor(word.length / 2);
-        result.push(
-            `<b>${word.slice(undefined, middle)}</b>${word.slice(
-                middle,
-                undefined
-            )}`
-        );
+    for (const te of targetElements) {
+        // console.log(te.textContent);
+        const words = te.textContent.split(' ');
+        let result = [];
+        for (const word of words) {
+            const middle = Math.floor(word.length / 2);
+            result.push(
+                `<b>${word.slice(undefined, middle)}</b>${word.slice(
+                    middle,
+                    undefined
+                )}`
+            );
+        }
+
+        te.innerHTML = result.join(' ');
+    }
+}
+
+// transformText()
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // const notification = document.getElementsByClassName('notification')[0];
+    // const notificationText = notification.getElementsByTagName('p')[0];
+
+    // notificationText.innerHTML = 'You are at: ' + request.tabTitle;
+
+    // notification.style.display = 'flex';
+
+    // setTimeout(function () {
+    //     notification.style.display = 'none';
+    // }, 5000);
+
+    console.log(request)
+
+    if(request.transform) {
+        transformText()
+
+    } else if (request.reload) {
+        location.reload()
     }
 
-    te.innerHTML = result.join(' ');
-}
+    return true;
+});
+
 
 // // Notification body.
 // const notification = document.createElement('div');
--- popup.js
+++ popup.js
@@ -2,6 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const dialogBox = document.getElementById('dialog-box');
     const query = { active: true, currentWindow: true };
 
+    document.getElementById("transformButton").onclick = () => {
+        const query = { active: true, currentWindow: true };
+        document.getElementById("transformButton").style.display = "none"
+        document.getElementById("reloadButton").style.display = "block"
+        chrome.tabs.query(query, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                transform: true,
+            });
+        });
+    }
+
+    document.getElementById("reloadButton").onclick = () => {
+        const query = { active: true, currentWindow: true };
+        document.getElementById("transformButton").style.display = "block"
+        document.getElementById("reloadButton").style.display = "none"
+        chrome.tabs.query(query, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                reload: true,
+            });
+        });
+    }
+
+
     chrome.tabs.query(query, (tabs) => {
         dialogBox.innerHTML = getBarkedTitle(tabs[0].title);
     });
