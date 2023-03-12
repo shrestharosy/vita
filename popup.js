e0d951dc445d7139a384e2d41d9b1d2a31c6f2bd
rosy <rosyshrestha028@gmail.com>
2023-03-11 23:12:54 -0500

    Cleanup

--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-
     const dialogBox = document.getElementById('dialog-box');
     const query = { active: true, currentWindow: true };
 
@@ -10,30 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 /**
- * Concatenates the tab title with Acho's barks.
+ *
  * @param {String} tabTitle Current tab title
  */
 const getBarkedTitle = (tabTitle) => {
-    const barkTitle = `${getRandomBark()} Ahem.. I mean, we are at: ${tabTitle}`
+    const barkTitle = `We are at ${tabTitle}`;
     return barkTitle;
-}
-
-/**
- * List of possible barks.
- */
-const barks = [
-    'Barf barf!',
-    'Birf birf!',
-    'Woof woof!',
-    'Arf arf!',
-    'Yip yip!',
-    'Biiiirf!'
-]
-
-/**
- * Returns a random bark from the list of possible barks.
- */
-const getRandomBark = () => {
-    const bark = barks[Math.floor(Math.random() * barks.length)];
-    return bark;
-}
+};
